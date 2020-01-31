a8fb5ed199f0d8ee924311902cb49d7f0fcf30b3
Jonathan Freitas <frejonathann@gmail.com>
2020-01-31 11:01:56 -0300

    Setting up project.
    Signed-off-by: Jonathan Freitas <frejonathann@gmail.com>

---
+++ MovieList/MovieList/Networking/MovieListNetwork.swift
@@ -0,0 +1,9 @@
+//
+//  MovieListNetwork.swift
+//  MovieList
+//
+//  Created by Jonathan Freitas Ferreira on 31/01/20.
+//  Copyright © 2020 Jonathan Freitas Ferreira. All rights reserved.
+//
+
+import Foundation
---
+++ MovieList/MovieList/UI/Main/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieList
+//
+//  Created by Jonathan Freitas Ferreira on 31/01/20.
+//  Copyright © 2020 Jonathan Freitas Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
